7df8db4f1143698148072f29c823d96be0de28be
Richard Leek <richard@richardleek.com>
2025-03-20 18:49:50 +0100

    Add methods

--- library/world.lua
+++ library/world.lua
@@ -13,7 +13,17 @@ function world.on_tick(callback) end
 
 ---Get an item based on it's name (Case-insensitive)
 ---@param name string The name of the item to get
----@return EifRecord|nil The matching item record, or nil if no match is found.
+---@return EifRecord|nil item The matching item record, or nil if no match is found.
 function world.get_item_by_name(name) end
+
+---Get an item based on it's name (Case-insensitive)
+---@param id integer The id of the item to get
+---@return EifRecord|nil item The matching item record, or nil if no match is found.
+function world.get_item_by_id(id) end
+
+---Get an item based on it's name (Case-insensitive)
+---@param name string The name of the item to get
+---@return integer|nil item_id The matching item id, or nil if no match is found.
+function world.get_item_id_by_name(name) end
 
 return world
